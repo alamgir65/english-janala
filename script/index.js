0d9d38b9777bf2d5fec54281b2d7d8d7b818fc8d
unknown <alamgirhossaindrubo@gmail.com>
2025-09-15 01:45:53 +0600

    Modal and Spinner added to the English Janala project

--- script/index.js
+++ script/index.js
@@ -8,19 +8,35 @@ loadLevels();
 
 const showLevels = (levels) => {
     const levelContainer = document.getElementById('level-container');
-    for(const level of levels){
+    for (const level of levels) {
         // console.log(level.lessonName);
 
         const button = document.createElement('button');
         button.classList.add('btn', 'btn-outline', 'btn-primary');
+        button.id = `level-btn-${level.level_no}`;
 
         button.onclick = () => {
             // const wordContainer = document.getElementById('word-container');
             // wordContainer.innerHTML = '';
 
+            manageSpinner(true);
+
             fetch(`https://openapi.programming-hero.com/api/level/${level.level_no}`)
                 .then(res => res.json())
                 .then(json => showWords(json));
+
+
+            // Active level button 
+            const thisButton = document.getElementById(`level-btn-${level.level_no}`);
+
+            const allButtons = document.querySelectorAll('#level-container button');
+            for (const btn of allButtons) {
+                btn.classList.remove('btn-primary');
+                btn.classList.add('btn-outline');
+            }
+
+            thisButton.classList.add('btn-primary');
+            thisButton.classList.remove('btn-outline');
 
             // alert(`You clicked on ${level.lessonName}`);
         };
@@ -34,12 +50,33 @@ const showLevels = (levels) => {
 
 const showWords = (json) => {
     // console.log(json.data);
+    // manageSpinner(true);
+    const words = json.data;
+
 
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = '';
 
+    if (words.length === 0) {
+        wordContainer.classList.remove('grid', 'grid-cols-1', 'md:grid-cols-3', 'gap-5');
 
-    for(const word of json.data){
+        wordContainer.innerHTML = `
+            <div class="text-center space-y-4">
+                <div>
+                    <img src="./assets/alert-error.png" alt="" class="mx-auto">
+                </div>
+                <p class="text-[8px]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h5 class="text-2xl">নেক্সট Lesson এ যান</h5>
+            </div>
+        `;
+        manageSpinner(false);
+        return;
+    }
+
+    wordContainer.classList.add('grid', 'grid-cols-1', 'md:grid-cols-3', 'gap-5');
+
+
+    for (const word of json.data) {
         console.log(word)
 
         const div = document.createElement('div');
@@ -47,17 +84,63 @@ const showWords = (json) => {
 
         div.innerHTML = `
                 <div class="text-center space-y-3 mb-5">
-                    <h2 class="text-xl font-semibold">${word.word}</h2>
+                    <h2 class="text-xl font-semibold">${word.word ? word.word : "Word not found!"}</h2>
                     <p class="text-lg ">Meaning/Pronounciation</p>
-                    <h5 class="font-bangla text-xl font-semibold opacity-80">"${word.meaning} / ${word.pronunciation}"</h5>
+                    <h5 class="font-bangla text-xl font-semibold opacity-80">"${word.meaning ? word.meaning : "Meaning not found!"} / ${word.pronunciation ? word.pronunciation : "Pronunciation not found!"}"</h5>
                 </div>
 
                 <div class="flex justify-between ">
-                    <div class="bg-sky-100 p-1 rounded-sm"><i class="fa-solid fa-circle-info"></i></div>
-                    <div class="bg-sky-100 p-1 rounded-sm"><i class="fa-solid fa-volume-high"></i></div>
+                    <button onclick="loadWordDetails(${word.id})" class="btn bg-sky-100 p-2 hover:bg-gray-500 rounded-sm"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-sky-100 p-2 hover:bg-gray-500 rounded-sm"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
         `;
 
         wordContainer.appendChild(div);
     }
+    manageSpinner(false);
 };
+
+const loadWordDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    showModal(details.data);
+}
+
+
+const manageSpinner = (isLoading) => {
+    const spinner = document.getElementById('sppiner');
+
+    if(isLoading){
+        spinner.classList.remove('hidden');
+        document.getElementById('word-container').classList.add('hidden');
+    }else{
+        spinner.classList.add('hidden');
+        document.getElementById('word-container').classList.remove('hidden');
+    }
+}
+
+const showModal = (word) => {
+    // console.log(word);
+
+    const modalBox = document.getElementById('modal-box');
+    modalBox.innerHTML = `
+        <h1 class="text-2xl font-bold mb-5">${word.word} (<i class="fa-solid fa-microphone-lines"></i>: ${word.pronunciation})</h1>
+                <div>
+                    <h3 class="text-xl font-semibold mb-2">Meaning:</h3>
+                    <p class="font-bangla text-lg opacity-80">${word.meaning}</p>
+                </div>
+                <div class="my-4">
+                    <h3 class="text-xl font-semibold mb-3">Example</h3>
+                    <p class="font-bangla text-lg opacity-80">${word.sentence}</p>
+                </div>
+                <div>
+                    <h3 class="text-xl font-bangla mb-2 font-semibold">সমার্থক শব্দ গুলো</h3>
+                    <div>
+                        ${word.synonyms.map(syn => `<button class="btn bg-sky-100 mr-2 mb-2">${syn}</button>`).join('')}
+                    </div>
+                </div>
+    `;
+
+    document.getElementById('detail_modal').showModal();
+}
